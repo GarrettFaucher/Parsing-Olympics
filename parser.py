6a7b2e1007f45e028d8dccaa2b244290d93eba68
Garrett Faucher <garrettfaucher@gmail.com>
2020-01-29 16:12:06 -0500

    Parse method implemented

--- parser.py
+++ parser.py
@@ -2,18 +2,22 @@
 
 
 # This method will split a string into an array of strings of keywords and
-# variables to be used in SQL queries
+# variables to be used in SQL queries.
+# Accepts: string
+# Returns: list
 def parse(input):
-
-
-
-    split[] = {}
+    split = input.split('\"')
+    i = 0
+    while i < len(split):
+        split[i] = split[i].strip()
+        i += 1
     return split
-    
+
+
 def print_welcome():
 
     welcomemsg = "\nThis program searches a database of the London 2012 Olympic gold medalists and the countries they are from. \n\nTo view a list of available commands, type 'help'.\n\nCommands follow the general format of <argument> <argument> <return>, e.g.: \n\n>Mexico swimming athlete \n\nwill return the list of Mexican athletes who medaled in swimming events."
-    
+
     print(welcomemsg)
     
 print_welcome()
