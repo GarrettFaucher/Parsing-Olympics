8f4ed50b9b9a7ce494c226a8a4459a2693ee4c4f
n0lley <ntolley13@gmail.com>
2020-01-29 15:40:00 -0500

    added welcome message

--- parser.py
+++ parser.py
@@ -1,1 +1,5 @@
 # THis is the file for the Python CLI
+def print_welcome():
+    
+    welcomemsg = "This program searches a database of the London 2012 Olympic gold medalists and the countries they are from. \n\nTo view a list of available commands, type 'help'."
+    
