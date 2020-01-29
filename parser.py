1a70f6dcdb8289c1ba8f0af15c86e39f283c5bed
Garrett Faucher <garrettfaucher@gmail.com>
2020-01-29 15:44:04 -0500

    Start parse method

--- parser.py
+++ parser.py
@@ -1,1 +1,11 @@
 # THis is the file for the Python CLI
+
+
+# This method will split a string into an array of strings of keywords and
+# variables to be used in SQL queries
+def parse(input):
+
+
+
+    split[] = {}
+    return split
