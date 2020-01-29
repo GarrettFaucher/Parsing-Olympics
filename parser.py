b135e2d7e64bc98834566fbab7f5c99f43da94e7
n0lley <ntolley13@gmail.com>
2020-01-29 16:03:31 -0500

    cleaned up welcome

--- parser.py
+++ parser.py
@@ -12,7 +12,7 @@ def parse(input):
     
 def print_welcome():
 
-    welcomemsg = "This program searches a database of the London 2012 Olympic gold medalists and the countries they are from. \n\nTo view a list of available commands, type 'help'.\n\nCommands follow the general format of <argument> <argument> <return>, e.g. \n\n>Mexico swimming athlete \n\nwill return the list of Mexican athletes who medaled in swimming events."
+    welcomemsg = "\nThis program searches a database of the London 2012 Olympic gold medalists and the countries they are from. \n\nTo view a list of available commands, type 'help'.\n\nCommands follow the general format of <argument> <argument> <return>, e.g.: \n\n>Mexico swimming athlete \n\nwill return the list of Mexican athletes who medaled in swimming events."
     
     print(welcomemsg)
     
